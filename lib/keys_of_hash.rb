bd9b6f906c44691953a73f1d5ee988909e7e505a
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 05:41:46 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,23 +2,10 @@ class Hash
   def keys_of(*arguments)
     array = []
     
-    self.each do |key, value|
-      if value == arguments
-        array << key
-      end
-    end
-    
-    array
-  end
-end
-
-class Hash
-  def keys_of(*arguments)
-    array = []
-    
     arguments.each do |string|
-      self.each do ||
-      array << self.key(string)
+      self.each do |key, value|
+        array << key if value == string
+      end
     end
     
     array
